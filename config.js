ddd83bb04519f487c9b9f14f5487583743bc2661
willardtenantsassociation <admin@wtassociation.org>
2026-03-26 18:35:37 -0400

    Update config.js
    replaced test links with live links

--- config.js
+++ config.js
@@ -12,16 +12,16 @@
 const PAYMENT_LINKS = {
   // One-time donation links
   onetime: {
-    10:     "https://buy.stripe.com/test_6oU6oG1yZaOI1mn7t5es000",
-    15:     "https://donate.stripe.com/test_9B6dR86Tjf4YfddeVxes001",
-    20:     "https://donate.stripe.com/test_4gM5kCcdD2icd5500Des002",
+    10:     "https://donate.stripe.com/cNiaEWgsd80y3Hy1V40ZW07",
+    15:     "https://donate.stripe.com/4gM7sKek56Wuce40R00ZW06",
+    20:     "https://donate.stripe.com/dRm9AS4Jv3Ki1zqczI0ZW05",
   },
 
   // Annual recurring donation links
   annual: {
-    10:     "https://donate.stripe.com/test_8x28wO0uVf4Yd557t5es003",
-    15:     "https://donate.stripe.com/test_14AdR8a5vf4Yd559Bdes004",
-    20:     "https://donate.stripe.com/test_9B614m6Tj9KEd554gTes005",
+    10:     "https://donate.stripe.com/4gMaEWek594C3HybvE0ZW04",
+    15:     "https://donate.stripe.com/28E9AS8ZLa8G2DugPY0ZW03",
+    20:     "https://donate.stripe.com/bJe00i5Nz0y63HygPY0ZW02",
   },
 };
 
